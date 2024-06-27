3bee1f1988d56708aaae5d62f7e72739eda5dd79
iambodha <165776420+iambodha@users.noreply.github.com>
2024-06-27 15:36:46 +0200

    Creating Face Boxes

--- faceAI.py
+++ faceAI.py
@@ -8,6 +8,20 @@ def faceAnalysis():
     display(image)
     print(DeepFace.analyze(img_path=imagePath, detector_backend='retinaface'))
 
+def faceBox():
+    imagePath = "RDJ.jpg"
+    image = Image.open(imagePath)
+    analysis = DeepFace.analyze(img_path=imagePath, detector_backend='retinaface')
+
+    region = analysis[0]['region']
+    draw = ImageDraw.Draw(image)
+    draw.rectangle([(region['x'], region['y']), (region['x'] + region['w'], region['y'] + region['h'])], outline="green", width=3)
+    modifiedImagePath = "RDJ_Modified.jpg"
+    image.save(modifiedImagePath)
+
+    image = Image.open(modifiedImagePath)
+    display(image)
+
 def verifySamePerson():
     print(DeepFace.verify(img1_path="RDJ.jpg", img2_path="RDJ2.jpg"))
 
